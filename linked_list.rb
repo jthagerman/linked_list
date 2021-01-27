45ba88eacfe482966522c151556506eba436c597
John Hagerman <jthagerma@gmail.com>
2021-01-26 22:06:04 -0500

    setup structure, got list working

--- linked_list.rb
+++ linked_list.rb
@@ -2,43 +2,58 @@ class Linked_list
 
     require_relative 'node'
 
-    #adds a new node containing value to the end of the list
-
     def initialize()
         @head = nil
         @tail = nil
     end
 
-
-
+    #add value to end of list
     def append(value)
         if @head == nil
             @head = value   
             @tail = value   
         else 
-            puts "hit"
-            puts value
+
             @tail.next_node = value
             @tail = value   
-        end
-        
+        end      
     end 
 
+    #adds value to beginning of the list
     def prepend(value) 
-        #adds a new node containing value to the start of the list
+        if @head == nil
+            @head = value   
+            @tail = value   
+        else 
+            value.next_node = @head
+            @head = value    
+        end   
     end
 
-    def size 
-        #returns the total number of nodes in the list
+    def head
+        return @head
     end
-
-    def head 
-        #returns the first node in the list
-    end
-
     def tail
-        # returns the last node in the list
+        return @tail
     end
+
+     #returns the total number of nodes in the list
+    def size 
+        if @head == nil
+            return 0
+        elsif @head.next_node == nil
+            return 1
+        else 
+            count = 2
+            node = @head.next_node
+            while node.next_node != nil
+                count += 1
+                node = node.next_node
+            end
+            return count
+        end     
+    end
+
 
     def at(index) 
         #returns the node at the given index
@@ -52,25 +67,42 @@ class Linked_list
         #returns true if the passed in value is in the list and otherwise returns false.
     end
 
-    def head
-        return @head
-    end
-
     def find(value) 
         #returns the index of the node containing value, or nil if not found.
     end
 
     def to_s
+        node = @head
+        str = ""
+        while node.next_node != nil
+            str += "( #{node.value} ) -> "
+            node = node.next_node
+        end
+        str += "( #{@tail} ) -> nil"
+        return str
         #represent your LinkedList objects as strings, so you can print them out and preview them in the console. The format should be: ( value ) -> ( value ) -> ( value ) -> nil
     end
 
 end
 
 
-a = Node.new("hello")
-c = Node.new("pizza")
-b = Linked_list.new()
-b.append(a)
-b.append(c)
-puts b.head
-puts b.tail
+a = Node.new("node 1")
+c = Node.new("node 2")
+f = Node.new('hello')
+g = Node.new('node 0')
+
+list = Linked_list.new()
+list.append(a)
+list.append(c)
+list.append(f)
+
+puts list.head
+puts list.tail
+
+list.prepend(g)
+
+puts list.head
+puts list.tail
+puts list.size
+
+puts list
--- node.rb
+++ node.rb
@@ -6,16 +6,11 @@ class Node
     def initialize(value = nil,next_node = nil)
         @next_node = next_node
         @value = value
-
     end
 
     def to_s
-        if(@next_node == nil)
-            return "[#{@value} , Next Node: null]"
-        else
-            return "[#{@value} , Next node: #{@next_node.value}]"
-        end
-    end  
+        return @value
+    end
 end
 
 
